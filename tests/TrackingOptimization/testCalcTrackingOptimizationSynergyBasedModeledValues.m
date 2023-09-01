8c7e13cc00ae30b0577721628a06fdff88d89cb9
stwilliams333 <stwilliamsmail@gmail.com>
2023-09-01 16:42:31 -0500

    Revert "Merge pull request #10 from rcnl-org/dev"
    This reverts commit a01b0de94a7530b03597b9e6abbf9bc737833c11, reversing
    changes made to 63a4c54ddb5029283ccc286210579a0f7112c55f.

--- tests/TrackingOptimization/testCalcTrackingOptimizationSynergyBasedModeledValues.m
+++ tests/TrackingOptimization/testCalcTrackingOptimizationSynergyBasedModeledValues.m
@@ -2,7 +2,7 @@ load('trackingOptimizationContinuousInputsAndValues.mat')
 expectedPhaseout = load('expectedPhaseout.mat');
 
 phaseout = calcSynergyBasedModeledValues(values, inputs.auxdata, expectedPhaseout);
-assertWithinRange(squeeze(phaseout.normalizedFiberLength)', expectedPhaseout.normalizedFiberLength, 1e-5);
-assertWithinRange(squeeze(phaseout.normalizedFiberVelocity)', expectedPhaseout.normalizedFiberVelocity, 6e-2);
+assertWithinRange(phaseout.normalizedFiberLength, expectedPhaseout.normalizedFiberLength, 1e-5);
+assertWithinRange(phaseout.normalizedFiberVelocity, expectedPhaseout.normalizedFiberVelocity, 6e-2);
 assertWithinRange(phaseout.muscleActivations, expectedPhaseout.muscleActivations, 1e-5);
 assertWithinRange(phaseout.muscleJointMoments, expectedPhaseout.muscleJointMoments, 2e1);
